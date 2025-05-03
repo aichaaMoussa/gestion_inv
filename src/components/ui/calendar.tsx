a3f04770f828dc4a941454808bbeb7b6468d6477
Aicha Ahmedou Moussa <aichaahmedou113@gmail.com>
2025-05-03 19:22:05 +0000

    fix table4

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" key="chevron-left" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" key="chevron-right" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
       }}
       {...props}
     />
