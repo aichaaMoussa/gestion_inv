ba61bcecb58a423ddbb91a2368c96b3cb7befce9
Aicha Ahmedou Moussa <aichaahmedou113@gmail.com>
2025-05-06 01:05:35 +0000

    fix red2"

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -152,17 +152,6 @@ export const authOptions: AuthOptions = {
 
   secret: process.env.NEXTAUTH_SECRET,
   debug: true, // Activer le mode debug en production
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true
-      }
-    }
-  }
 };
 
 export default NextAuth(authOptions);
