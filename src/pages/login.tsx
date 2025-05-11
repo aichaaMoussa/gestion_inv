e4984e92c8a550c21509838af2e7a05aa9b22b18
Aicha Ahmedou Moussa <aichaahmedou113@gmail.com>
2025-05-11 18:24:24 +0000

    fix:login2

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -47,14 +47,7 @@ function Login() {
     }
   };
 
-  if (status === "authenticated") {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        Redirection...
-      </div>
-    );
-  }
-
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
